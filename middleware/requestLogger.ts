9a3e9941b6cfc1b099b696d5bbb4f86d93120e06
James Pietras <james.m.pietras@gmail.com>
2017-03-26 23:26:17 +0200

    Request logger update

--- middleware/requestLogger.ts
+++ middleware/requestLogger.ts
@@ -1,6 +1,7 @@
-import * as http from 'http';
+import { Request, Response } from 'express';
 import * as responseTime from 'response-time';
 import * as requestIp from 'request-ip';
+import * as nodeUrl from 'url';
 
 import logger from 'modules/logger';
 
@@ -9,13 +10,17 @@ function hook() {
 }
 
 function requestLogger(): any {
-  return responseTime(function(
-    req: http.IncomingMessage, res: http.ServerResponse, time: number
-  ): any {
+  return responseTime(function(req: Request, res: Response, time: number): any {
+    const urlObject: nodeUrl.URL = new nodeUrl.URL(nodeUrl.format({
+      host: req.get('host'),
+      pathname: req.originalUrl || req.url,
+      protocol: req.protocol
+    }));
+
     logger.server.info('Request logged.', {
       authorization: req.headers['Authorization'],
       clientIp: requestIp.getClientIp(req),
-      endpoint: req.url,
+      endpoint: urlObject.pathname,
       httpVersion: req.httpVersion,
       method: req.method,
       responseTime: `${time.toFixed(4)}ms`,
